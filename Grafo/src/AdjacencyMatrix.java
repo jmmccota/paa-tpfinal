f9602637179979d64fddacc2c002558a173907bd
João <joao_marcos_cota@hotmail.com>
2017-07-05 19:59:22 -0300

    Matriz de adjacencia

--- Grafo/src/AdjacencyMatrix.java
+++ Grafo/src/AdjacencyMatrix.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -29,18 +31,32 @@ public class AdjacencyMatrix {
 		return -1;
 	}
 
-	public static void main(String args[]) {
-		int v, e, count = 1;
+	public void saveFile(String file) throws Exception {
+		FileWriter writer = new FileWriter(file + "Matrix.txt");
+		PrintWriter print = new PrintWriter(writer);
+		int v = vertices;
+		for (int i = 1; i <= v; i++) {
+			// System.out.print(i + " ");
+			for (int j = 1; j <= v; j++) {
+				print.printf(getEdge(i, j) + " ");
+				// System.out.print(graph.getEdge(i, j) + " ");
+			}
+			print.printf("\r\n");
+			// System.out.println();
+		}
+		print.close();
+		writer.close();
+	}
+
+	public static void carregarSBBD() {
+		int v;
 		AdjacencyMatrix graph;
 		try {
 
 			v = 1480;
 
-			e = 1480;
-
 			graph = new AdjacencyMatrix(v);
 			String file = "C:/Users/JMMCC/Downloads/Trabalho Final/Workspace/work_author.csv";
-			System.out.println("Enter the edges: <to> <from>");
 			Data d = new Data();
 			d.readData(file);
 			int maior = 0;
@@ -56,29 +72,54 @@ public class AdjacencyMatrix {
 				}
 			}
 			System.out.println(maior);
+			System.out.println("Enter the edges: <to> <from>");
 
 			System.out.println("The adjacency matrix for the given graph is: ");
 			System.out.print("  ");
 			// for (int i = 1; i <= v; i++)
 			// System.out.print(i + " ");
 			// System.out.println();
-			FileWriter writer = new FileWriter(file + "Matrix.txt");
-			PrintWriter print = new PrintWriter(writer);
-			for (int i = 1; i <= v; i++) {
-				// System.out.print(i + " ");
-				for (int j = 1; j <= v; j++) {
-					print.printf(graph.getEdge(i, j) + " ");
-					// System.out.print(graph.getEdge(i, j) + " ");
-				}
-				print.printf("\r\n");
-				// System.out.println();
-			}
-			print.close();
-			writer.close();
+
+			graph.saveFile(file);
 
 		} catch (Exception E) {
 			System.out.println("Somthing went wrong");
 		}
+	}
+	
+	
+	public static void carregarBenchmark(String file) {
+		AdjacencyMatrix graph;
+		try {
+
+			
+			dimacs.Data d = new dimacs.Data();
+			int v = d.readData(file);
+			System.out.println(v);
+			graph = new AdjacencyMatrix(v);
+			for (int i = 0; i<d.u.size(); i++){
+				for(int j = i+1; j < d.v.size(); j++){
+					graph.makeEdge(d.u.get(i), d.v.get(j), 1);
+					graph.makeEdge(d.v.get(i), d.u.get(j), 1);
+				}
+			}
+			
+			
+			// for (int i = 1; i <= v; i++)
+			// System.out.print(i + " ");
+			// System.out.println();
+
+			graph.saveFile(file);
+
+		} catch (Exception E) {
+			System.out.println("Somthing went wrong");
+		}
+	}
+
+	public static void main(String args[]) {
+
+		//carregarSBBD();
+		carregarBenchmark("C:/Users/JMMCC/Downloads/Trabalho Final/Workspace/Dados/johnson8-4-4.txt");
 
 	}
 }
