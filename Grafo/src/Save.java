f2aef13f0f0bafe4b618178fe31f69875da498c6
João <joao_marcos_cota@hotmail.com>
2017-07-04 21:17:06 -0300

    Separação

---
+++ Grafo/src/Save.java
@@ -0,0 +1,113 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class Save {
+	private String data;
+
+	public Save(String data) {
+		this.data = data;
+	}
+
+	public void saveClique(ArrayList<Integer> cliqueVertex) {
+		try {
+			FileWriter writer = new FileWriter(data + "_Clique.txt");
+			PrintWriter print = new PrintWriter(writer);
+
+			for (int i = cliqueVertex.size() - 1; i >= 0; i--)
+				print.printf(cliqueVertex.get(i) + "\r\n");
+
+			print.close();
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("Sorry, it was not possible to save the file.\r\nERROR %s.\r\n", e.getMessage());
+		}
+	}
+
+	public void saveMedicoes(long tg, long tc, long mt1, long mf1, long mt2, long mf2) {
+		try {
+			FileWriter writer = new FileWriter(data + "_Medicoes.txt");
+			PrintWriter print = new PrintWriter(writer);
+
+			print.printf("Time Graph: " + tg + "\r\n");
+			print.printf("Time Clique: " + tc + "\r\n");
+			print.printf("Memory Total 1: " + mt1 + "\r\n");
+			print.printf("Memory Free 1: " + mf1 + "\r\n");
+			print.printf("Memory Total 2: " + mt2 + "\r\n");
+			print.printf("Memory Free 2: " + mf2 + "\r\n");
+
+			print.close();
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("Sorry, it was not possible to save the file.\r\nERROR %s.\r\n", e.getMessage());
+		}
+	}
+
+	public void saveOthers(int access, int cliqueSize, int lowerBound, Graph g) {
+		float degree = 0;
+		for (int i = 0; i < g.n; i++)
+			degree += g.vertex.get(i).degree;
+		degree /= g.n;
+
+		try {
+			FileWriter writer = new FileWriter(data + "_Others.txt");
+			PrintWriter print = new PrintWriter(writer);
+
+			print.printf("Access: " + access + "\r\n");
+			print.printf("Clique Size: " + cliqueSize + "\r\n");
+			print.printf("Lower Bound: " + lowerBound + "\r\n");
+			print.printf("Average Degree: " + degree + "\r\n");
+
+			print.close();
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("Sorry, it was not possible to save the file.\r\nERROR %s.\r\n", e.getMessage());
+		}
+	}
+
+	public void saveOthers(int access, int cliqueSize, int lowerBound, int upperBound, Graph g) {
+		float degree = 0;
+		for (int i = 0; i < g.n; i++)
+			degree += g.vertex.get(i).degree;
+		degree /= g.n;
+
+		try {
+			FileWriter writer = new FileWriter(data + "_Others.txt");
+			PrintWriter print = new PrintWriter(writer);
+
+			print.printf("Access: " + access + "\r\n");
+			print.printf("Clique Size: " + cliqueSize + "\r\n");
+			print.printf("Lower Bound: " + lowerBound + "\r\n");
+			print.printf("Upper Bound: " + lowerBound + "\r\n");
+			print.printf("Average Degree: " + degree + "\r\n");
+
+			print.close();
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("Sorry, it was not possible to save the file.\r\nERROR %s.\r\n", e.getMessage());
+		}
+	}
+
+	public void saveOthers(int access, int cliqueSize, Graph g) {
+		float degree = 0;
+		for (int i = 0; i < g.n; i++)
+			degree += g.vertex.get(i).degree;
+		degree /= g.n;
+
+		try {
+			FileWriter writer = new FileWriter(data + "_Others.txt");
+			PrintWriter print = new PrintWriter(writer);
+
+			print.printf("Access: " + access + "\r\r\n");
+			print.printf("Clique Size: " + cliqueSize + "\r\r\n");
+			print.printf("Average Degree: " + degree + "\r\r\n");
+
+			print.close();
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("Sorry, it was not possible to save the file.\r\nERROR %s.\r\n", e.getMessage());
+		}
+	}
+}
